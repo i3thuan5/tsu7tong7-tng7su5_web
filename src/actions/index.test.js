08c76b4774c41aaf6ab0ee952ca69d658e22737f
pigu <a8568730@gmail.com>
2018-06-19 17:29:54 +0800

    實做收著錯誤訊息。

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -9,13 +9,14 @@ const middlewares = [thunk];
 const mockStore = configMockStore(middlewares);
 
 
-it('要傳 語句 辭典', () => {
+it('要傳 語句 辭典', async () => {
   let 語句 = 'sui2';
   let fakeStore = mockStore({
     查: {查詢結果: []},
     辭典: {辭典表: []}
   });
-  return fakeStore
+
+  await fakeStore
     .dispatch(查(語句))
     .then(() => {
       expect(查ajax).toBeCalledWith(語句, []);
@@ -37,5 +38,23 @@ it('收到查詢結果', async () => {
     const actions = fakeStore.getActions();
     expect(actions).toContainEqual({type: 'TSHA', 狀態: 'KHAI_SI'});
     expect(actions).toContainEqual({type: 'TSHA', 狀態: 'SING_KONG', 查詢結果:{綜合標音:[]}});
+  });
+});
+
+
+it('收到錯誤訊息', async () => {
+  let 語句 = 'bai2';
+  let 辭典 = [];
+  let fakeStore = mockStore({
+    查: {查詢結果: []},
+    辭典: {辭典表: []}
+  });
+
+  await fakeStore
+  .dispatch(查(語句, 辭典))
+  .then(() => {
+    const actions = fakeStore.getActions();
+    expect(actions).toContainEqual({type: 'TSHA', 狀態: 'KHAI_SI'});
+    expect(actions).toContainEqual({type: 'TSHA', 狀態: 'SIT_PAI', 錯誤訊息: 'oh no'});
   });
 });
