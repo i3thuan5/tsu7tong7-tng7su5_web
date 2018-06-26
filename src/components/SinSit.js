2228727b7375e22c56229f7f080445cab5f11627
a8568730 <a8568730@hotmail.com>
2018-06-26 10:55:59 +0800

    錯誤訊息是空字串時要隱藏訊息元件

--- src/components/SinSit.js
+++ src/components/SinSit.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const SinSit = ({訊息, 樣式='negative'}) =>
+const SinSit = ({訊息, 樣式='negative'}) => {
+
+  if(!訊息) return null
+
+  return (
   <div className={`ui ${樣式} message`}>
     <p>{訊息}</p>
   </div>
+)}
 
 export default SinSit;
