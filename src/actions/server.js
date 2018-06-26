d3d5736bbdc79e012c556d6272c90c7be744cb88
a8568730 <a8568730@hotmail.com>
2018-06-26 23:04:37 +0800

    修正axios傳參數方式

--- src/actions/server.js
+++ src/actions/server.js
@@ -2,8 +2,9 @@ import axios from 'axios'
 
 export const 查ajax = (語句, 辭典) => axios.get(
   "https://服務.意傳.台灣/標漢羅", {
+    params: {
     查詢腔口: '台語',
     查詢語句: 語句,
-    使用者辭典: 辭典
-  })
+    使用者辭典: JSON.stringify(辭典)
+  }})
 
